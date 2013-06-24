fe7822d04f422ac82bcdb1c01efe2d3f455d2e40
david raygoza <david_raygoza@hotmail.com>
2013-06-24 18:10:07 -0500

    The begin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HeadFirtsPersonalTraining::Application.config.secret_token = 'ca4806ac58ef364b1d4b1dc865082cd2eb3843eda6b9393639cb0142e81734114f71cc3ec18dcbcd9f9eb27764a638202a7aca1ecf8bec5fe1fe252484aa26aa'
